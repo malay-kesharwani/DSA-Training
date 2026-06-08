b95607ad023613adcafe50e9ac9ee0539186359c
malay-kesharwani <malaykesharwani26@gmail.com>
2026-06-08 16:52:54 +0530

    Create besttime.java

---
+++ Week1/DAy1/besttime.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class BestTimeToBuyAndSellStock {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] prices = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            prices[i] = sc.nextInt();
+        }
+
+        int minPrice = prices[0];
+        int maxProfit = 0;
+
+        for (int i = 1; i < n; i++) {
+            if (prices[i] < minPrice) {
+                minPrice = prices[i];
+            } else {
+                maxProfit = Math.max(maxProfit, prices[i] - minPrice);
+            }
+        }
+
+        System.out.println(maxProfit);
+    }
+}
