c09ba911a9bd155a194e44f75067ccf6aab64a3f
malay-kesharwani <malaykesharwani26@gmail.com>
2026-06-09 14:31:02 +0530

    Create firstuniquecharinastring.java

---
+++ Week2/Day1/firstuniquecharinastring.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class FirstUniqueCharacterInAString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.nextLine();
+
+        int[] freq = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            freq[s.charAt(i) - 'a']++;
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            if (freq[s.charAt(i) - 'a'] == 1) {
+                System.out.println(i);
+                return;
+            }
+        }
+
+        System.out.println(-1);
+    }
+}
