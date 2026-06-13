5de037b45bc76de68b702718076a010581429801
malay-kesharwani <malaykesharwani26@gmail.com>
2026-06-13 21:42:02 +0530

    Create test.java

---
+++ Week2/OOPS/test.java
@@ -0,0 +1,10 @@
+public class Test {
+    public static void main(String[] args) {
+
+        Veena veena = new Veena();
+        Saxophone saxophone = new Saxophone();
+
+        veena.play();
+        saxophone.play();
+    }
+}
