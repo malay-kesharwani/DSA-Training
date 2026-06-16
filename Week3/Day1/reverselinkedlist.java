19f42405a59bd592dd9f715a3e5a5e2c0359888f
malay-kesharwani <malaykesharwani26@gmail.com>
2026-06-16 17:21:10 +0530

    Create reverselinkedlist.java

---
+++ Week3/Day1/reverselinkedlist.java
@@ -0,0 +1,39 @@
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+    }
+}
+
+public class ReverseLinkedList {
+
+    static Node reverse(Node head) {
+        Node prev = null;
+        Node curr = head;
+
+        while (curr != null) {
+            Node next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        return prev;
+    }
+
+    public static void main(String[] args) {
+
+        Node head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+
+        head = reverse(head);
+
+        while (head != null) {
+            System.out.print(head.data + " ");
+            head = head.next;
+        }
+    }
+}
