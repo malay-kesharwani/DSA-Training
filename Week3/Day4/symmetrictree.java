905cddba7ca8d66d92a79b24cb88a5e59137d560
malay-kesharwani <malaykesharwani26@gmail.com>
2026-06-21 11:22:15 +0530

    Implement Symmetric Tree check in Java

---
+++ Week3/Day4/symmetrictree.java
@@ -0,0 +1,52 @@
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+}
+
+public class SymmetricTree {
+
+    static boolean isMirror(TreeNode left, TreeNode right) {
+
+        if (left == null && right == null) {
+            return true;
+        }
+
+        if (left == null || right == null) {
+            return false;
+        }
+
+        return left.val == right.val
+                && isMirror(left.left, right.right)
+                && isMirror(left.right, right.left);
+    }
+
+    static boolean isSymmetric(TreeNode root) {
+
+        if (root == null) {
+            return true;
+        }
+
+        return isMirror(root.left, root.right);
+    }
+
+    public static void main(String[] args) {
+
+        TreeNode root = new TreeNode(1);
+
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(2);
+
+        root.left.left = new TreeNode(3);
+        root.left.right = new TreeNode(4);
+
+        root.right.left = new TreeNode(4);
+        root.right.right = new TreeNode(3);
+
+        System.out.println(isSymmetric(root));
+    }
+}
